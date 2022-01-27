a40d13928108ea8ee7961ee5a87630f9b5adac7a
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-27 09:54:25 -0800

    added comments and refactored the code

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,9 @@ function isTouching(a, b) {
 
 let currentLeft = 100;
 let currentTop = 100;
+// const currentLeft = parseInt(mario.style.left) || 100
+// const currentTop = parseInt(mario.style.top) || 100
+
 let score = 0;
 let direction = true;
 let marioWidth = 100;
@@ -21,6 +24,16 @@ const coin = document.querySelector('#coin');
 const scoreboard = document.querySelector('#scoreboard');
 console.dir(mario);
 
+/*
+// Get the css style that a node has
+console.log(getComputedStyle(mario).top); // 100px
+console.log(parseInt(getComputedStyle(mario).top)); // 100
+*/
+
+const coinOffSet = coin.width;
+const maxWidth = window.innerWidth;
+const maxHeight = window.innerHeight;
+
 function flipMario(direction) {
   if (!direction) {
     mario.style.transform = 'scale(-1, 1)';
@@ -29,56 +42,64 @@ function flipMario(direction) {
   }
 }
 
-window.addEventListener('keydown', e => {
-  e.preventDefault();
+const init = () => {
+  window.addEventListener('keydown', e => {
+    e.preventDefault();
 
-  // Get Mario moving around the screen
-  switch (e.key) {
-    case 'ArrowRight':
-      currentLeft < window.innerWidth - 100
-        ? (currentLeft += step)
-        : currentLeft;
-      mario.style.left = `${currentLeft}px`;
-      direction = true;
-      flipMario(direction);
-      break;
-    case 'ArrowLeft':
-      currentLeft > 0 ? (currentLeft -= step) : currentLeft;
-      mario.style.left = `${currentLeft}px`;
-      mario.style.transform = 'scale(-1, 1)';
-      direction = false;
-      flipMario(direction);
-      break;
-    case 'ArrowUp':
-      currentTop > 0 ? (currentTop -= step) : currentTop;
-      mario.style.top = `${currentTop}px`;
-      break;
-    case 'ArrowDown':
-      currentTop < window.innerHeight - 100 ? (currentTop += step) : currentTop;
-      mario.style.top = `${currentTop}px`;
-      break;
+    // Get Mario moving around the screen
+    switch (e.key) {
+      case 'ArrowRight':
+        currentLeft + step < maxWidth - coinOffSet
+          ? (currentLeft += step)
+          : currentLeft;
+        mario.style.left = `${currentLeft}px`;
+        direction = true;
+        flipMario(direction);
+        break;
+      case 'ArrowLeft':
+        currentLeft + step >= 0 ? (currentLeft -= step) : currentLeft;
+        mario.style.left = `${currentLeft}px`;
+        mario.style.transform = 'scale(-1, 1)';
+        direction = false;
+        flipMario(direction);
+        break;
+      case 'ArrowUp':
+        currentTop + step >= 0 ? (currentTop -= step) : currentTop;
+        mario.style.top = `${currentTop}px`;
+        break;
+      case 'ArrowDown':
+        currentTop + step < maxHeight - coinOffSet
+          ? (currentTop += step)
+          : currentTop;
+        mario.style.top = `${currentTop}px`;
+        break;
 
-    default:
-      console.log("can't move...");
-  }
+      default:
+        console.log("can't move...");
+    }
 
-  // Get coin moving randomly in the screen
-  if (isTouching(mario, coin)) {
-    const height = Math.floor(Math.random() * (window.innerHeight - 100));
-    const width = Math.floor(Math.random() * (window.innerWidth - 100));
+    // Get coin moving randomly in the screen
+    if (isTouching(mario, coin)) {
+      const height = Math.floor(Math.random() * (maxHeight - coinOffSet));
+      const width = Math.floor(Math.random() * (maxWidth - coinOffSet));
 
-    coin.style.top = `${height}px`;
-    coin.style.left = `${width}px`;
+      coin.style.top = `${height}px`;
+      coin.style.left = `${width}px`;
 
-    score += 10;
-    scoreboard.textContent = `Your Score : ${score}`;
+      score += 10;
+      scoreboard.textContent = `Your Score : ${score}`;
 
-    marioWidth += 30;
-    console.log(marioWidth);
-    mario.style.width = `${marioWidth}px`;
-    mario.style.height = 'auto';
+      marioWidth += 30;
+      console.log(marioWidth);
+      mario.style.width = `${marioWidth}px`;
+      mario.style.height = 'auto';
 
-    const sound = new Audio('/smw_coin.wav');
-    sound.play();
-  }
-});
+      const sound = new Audio('/smw_coin.wav');
+      sound.play();
+    }
+  });
+};
+
+window.onload = function () {
+  init();
+};
